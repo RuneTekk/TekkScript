3906512e472f46000de94ee216bef00ea6e75188
RunelocusDev <runelocusdev@gmail.com>
2012-03-20 21:29:56 -0600

    Testing parsing numeric and string values from context.

--- src/org/runetekk/Main.java
+++ src/org/runetekk/Main.java
@@ -12,6 +12,36 @@ public final class Main {
      * @param args The command line arguments.
      */
     public static void main(String[] args) {
-        
+        char[] chars = "if a do print(\"Hello\") end".toCharArray();
+        String[] integers = new String[500];
+        String[] strings = new String[500];
+        boolean active = false;
+        int offset = 0;
+        for(int i = 0; i < chars.length; i++) {
+            boolean lastState = active;
+            if(chars[i] == '\"')                
+                active = !active;
+            if(lastState && !active)
+                offset++;
+            if(lastState && active) {
+                if(strings[offset] == null)
+                    strings[offset] = "";
+                strings[offset] += chars[i];
+            }
+        }
+        active = false;
+        offset = 0;
+        for(int i = 0; i < chars.length; i++) {
+            boolean lastState = active;
+            if((chars[i] >= '0' && chars[i] <= '9') ^ active)                
+                active = !active;
+            if(lastState && !active)
+                offset++;
+            if(active) {
+                if(integers[offset] == null)
+                    integers[offset] = "";
+                integers[offset] += chars[i];
+            }
+        } 
     } 
 }
